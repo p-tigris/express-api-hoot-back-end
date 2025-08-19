bb601a89b83f572e31abcc89e0de26e919411332
smdhar <shoumik_dhar@yahoo.com>
2025-08-19 15:45:24 -0400

    hoot create and update comments functionality completed

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -80,6 +80,43 @@ router.delete("/:hootId", verifyToken, async (req, res) => {
     }
 });
 
+// POST - /hoots/:hootId/comments
+router.post('/:hootId/comments', verifyToken, async (req, res) => {
+    try {
+        req.body.author = req.user._id;
+        const hoot = await Hoot.findById(req.params.hootId);
+        hoot.comments.push(req.body);
+        await hoot.save();
+        const newComment = hoot.comments[hoot.comments.length - 1];
+        newComment._doc.author = req.user;
+        res.status(201).json(newComment);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// PUT - /hoots/:hootId/comments/:commentId
+router.put("/:hootId/comments/:commentId", verifyToken, async (req, res) => {
+  try {
+    const hoot = await Hoot.findById(req.params.hootId);
+    const comment = hoot.comments.id(req.params.commentId);
+
+    // ensures the current user is the author of the comment
+    if (comment.author.toString() !== req.user._id) {
+      return res
+        .status(403)
+        .json({ message: "You are not authorized to edit this comment" });
+    }
+
+    comment.text = req.body.text;
+    await hoot.save();
+    res.status(200).json({ message: "Comment updated successfully" });
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+
 
 
 module.exports = router;
