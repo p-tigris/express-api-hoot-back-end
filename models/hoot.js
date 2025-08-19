b416954bd5039efc689f5c963e02eb3563cf2959
smdhar <shoumik_dhar@yahoo.com>
2025-08-19 11:42:37 -0400

    create hoot model completed

--- models/hoot.js
+++ models/hoot.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
 
-const commentSchema = new mongoose.Schema({
-    text: {
-        type: String,
-        required: true
+const commentSchema = new mongoose.Schema(
+    {
+        text: {
+            type: String,
+            required: true
+        },
+        author: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
     },
-    author: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    },
-     timestamps: true 
-});
+    { timestamps: true } 
+);
 
 const hootSchema = new mongoose.Schema(
   {
