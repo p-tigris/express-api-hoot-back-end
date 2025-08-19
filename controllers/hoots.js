0e595a4e9c7d01359e9d73f48212ded467fd0918
smdhar <shoumik_dhar@yahoo.com>
2025-08-19 13:07:20 -0400

    hoots index functionality completed

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -15,5 +15,14 @@ router.post("/", verifyToken, async(req, res) => {
     }
 });
 
+router.get('/', verifyToken, async(req, res) => {
+    try {
+        const hoots = await Hoot.find({}).populate('author').sort({ createdAt: 'desc' });
+
+        res.status(200).json(hoots);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
 
 module.exports = router;
