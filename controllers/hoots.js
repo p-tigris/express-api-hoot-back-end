068dfd5763b5e248c11df5a5a376b6c095e28d49
smdhar <shoumik_dhar@yahoo.com>
2025-08-19 14:58:22 -0400

    hoot delete functionality completed

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -64,5 +64,22 @@ router.put("/:hootId", verifyToken, async (req, res) => {
     }
 });
 
+// DELETE - /hoots/:hootId
+router.delete("/:hootId", verifyToken, async (req, res) => {
+    try {
+        const hoot = await Hoot.findById(req.params.hootId);
+
+        if (!hoot.author.equals(req.user._id)) {
+            return res.status(403).send('You\'re not allowed to do that!');
+        }
+
+        const deletedHoot = await Hoot.findByIdAndDelete(req.params.hootId);
+        res.status(200).json(deletedHoot);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
 
 module.exports = router;
